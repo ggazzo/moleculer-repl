52ed8923af76a5a4662c4214dba90d1893bb4b5c
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-08-23 17:06:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,22 @@
-module.exports = {
-
-
-	logLevel: 'info',
+const  { HEARTBEAT_INTERVAL = 10, HEARTBEAT_TIMEOUT = 30 } = process.env;
+export default {
+	logLevel: process.env.MOLECULER_LOG_LEVEL || "error",
 	sampleCount: 1,
 	metrics: true,
-	transporter: process.env.TRANSPORTER || 'nats://localhost:4222',
-	cacher: process.env.CACHE || 'Memory',
-	serializer: 'MsgPack',
+	transporter: process.env.TRANSPORTER || "nats://localhost:4222",
+	cacher: process.env.CACHE || "Memory",
+	serializer: process.env.SERIALIZER || "MsgPack",
 
 	registry: {
-		strategy: process.env.BALANCE_STRATEGY || 'Random',
-		preferLocal: process.env.BALANCE_PREFER_LOCAL || false,
+		strategy: process.env.BALANCE_STRATEGY || "Random",
+		preferLocal: process.env.BALANCE_PREFER_LOCAL !== "false"
 	},
 
-	namespace: '',
+	namespace: "",
 	nodeID: null,
 
 	logger: true,
-	logFormatter: 'default',
+	logFormatter: "default",
 	logObjectPrinter: null,
 
 	requestTimeout: 10 * 1000,
@@ -27,16 +26,16 @@ module.exports = {
 		delay: 100,
 		maxDelay: 1000,
 		factor: 2,
-		check: (err) => err && !!err.retryable,
+		check: err => err && !!err.retryable
 	},
 
 	maxCallLevel: 100,
-	heartbeatInterval: 5,
-	heartbeatTimeout: 15,
+	heartbeatInterval: parseInt(HEARTBEAT_INTERVAL),
+	heartbeatTimeout: parseInt(HEARTBEAT_TIMEOUT),
 
 	tracking: {
 		enabled: false,
-		shutdownTimeout: 5000,
+		shutdownTimeout: 5000
 	},
 
 	disableBalancer: false,
@@ -47,13 +46,17 @@ module.exports = {
 		windowTime: 60,
 		minRequestCount: 20,
 		halfOpenTime: 10 * 1000,
-		check: (err) => err && err.code >= 500,
+		check: err => err && err.code >= 500
 	},
 
 	bulkhead: {
-		enabled: false,
-		concurrency: 10,
-		maxQueueSize: 100,
+		enabled: process.env.BULKHEAD_ENABLED === "true",
+		concurrency: process.env.BULKHEAD_CONCURRENCY
+			? parseInt(process.env.BULKHEAD_CONCURRENCY)
+			: 10,
+		maxQueueSize: process.env.BULKHEAD_MAX_QUEUE_SIZE
+			? parseInt(process.env.BULKHEAD_MAX_QUEUE_SIZE)
+			: 10000
 	},
 
 	validation: true,
@@ -70,19 +73,26 @@ module.exports = {
 	middlewares: [],
 
 	// Called after broker created.
-	created(/* broker*/) {
-
+	created(broker) {
+		broker.getCpuUsage = async function() {
+			return {
+				avg: 1,
+				usages: [
+					1
+				]
+			};
+		};
 	},
 
-	// Called after broker starte.
-	started(/* broker*/) {
+	// // Called after broker starte.
+	// started(/* broker*/) {
 
-	},
+	// },
 
-	// Called after broker stopped.
-	stopped(/* broker*/) {
+	// // Called after broker stopped.
+	// stopped(/* broker*/) {
 
-	},
+	// },
 
-	replCommands: null,
+	replCommands: null
 };
